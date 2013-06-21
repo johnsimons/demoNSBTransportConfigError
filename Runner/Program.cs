4635a98bc8b73cc3f756d3d452a0a9510d9cff42
Karl Strauss <kstrauss@realgy.com>
2013-06-21 07:22:41 -0400

    Initial Commit

---
+++ Runner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using log4net.Config;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log4NetConfig = ConfigurationManager.AppSettings["log4netConfig"];
+            if (log4NetConfig != null && File.Exists(log4NetConfig))
+            {
+                XmlConfigurator.ConfigureAndWatch(new FileInfo(log4NetConfig));
+            }
+            else
+            {
+                Console.Error.WriteLine("Could not find log4net");
+            }
+            new Class1().Amethod();
+        }
+    }
+}
